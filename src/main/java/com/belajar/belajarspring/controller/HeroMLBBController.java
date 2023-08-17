8ac43b86b9ec957d374f9e98584e2f32d2270fcc
Tomi Saputra <tommycodingjj@gmail.com>
2023-08-17 13:14:01 +0700

    add get method

---
+++ src/main/java/com/belajar/belajarspring/controller/HeroMLBBController.java
@@ -0,0 +1,28 @@
+package com.belajar.belajarspring.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.model.Hero;
+
+@RestController
+@RequestMapping("/hero")
+public class HeroMLBBController {
+
+    public List<Hero> heroes = new ArrayList<>();
+
+    @GetMapping("/daftar-hero")
+    public List<Hero> daftarHero() {
+        heroes.add(new Hero(1, "balmond", 100));
+        heroes.add(new Hero(2, "miya", 200));
+        heroes.add(new Hero(3, "hanzo", 500));
+        heroes.add(new Hero(4, "tigreal", 500));
+        heroes.add(new Hero(5, "zilong", 300));
+        return heroes;
+    }
+
+}

9619a253a20c8f6c34b2896453e6aa9326863510
Tomi Saputra <tommycodingjj@gmail.com>
2023-08-20 19:48:09 +0700

    Add Interface and Service implementations

--- src/main/java/com/belajar/belajarspring/controller/HeroMLBBController.java
+++ src/main/java/com/belajar/belajarspring/controller/HeroMLBBController.java
@@ -1,8 +1,6 @@
 package com.belajar.belajarspring.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.model.Hero;
+import com.belajar.belajarspring.model.Hero;
 
 @RestController
 @RequestMapping("/hero")
@@ -23,65 +21,34 @@ public class HeroMLBBController {
 
     // menjadikan data model Hero menjadi list
     // lalu di tambahkan data listnya dalam function daftarHero
-    public List<Hero> heroes = new ArrayList<>();
-    public int counter = 1;
 
     // Read
     @GetMapping("/daftar-hero")
     public List<Hero> daftarHero() {
-        // menambah data secara manual
-        // heroes.add(new Hero(1, "balmond", 100));
-        // heroes.add(new Hero(2, "miya", 200));
-        // heroes.add(new Hero(3, "hanzo", 500));
-        // heroes.add(new Hero(4, "tigreal", 500));
-        // heroes.add(new Hero(5, "zilong", 300));
-        return heroes;
+
     }
 
     // Create
     @PostMapping(path = "tambah")
     public Hero tambahDataHero(@RequestBody Hero request) {
-        request.setId(counter); // auto add id
-        heroes.add(request);
-        counter++; // auto add id
-        return request;
 
     }
 
     // update
     @PutMapping(path = "/ubah/{id}")
     public Boolean updateData(@RequestBody Hero request, @PathVariable Integer id) {
-        final Optional<Hero> result = heroes.stream().filter(hero -> hero.getId() == id).findFirst();
-        if (result.isPresent()) {
-            result.get().setName(request.getName());
-            result.get().setDamage(request.getDamage());
-            return true;
-        } else {
-            return false;
-        }
+
     }
 
     // update partial data
     @PatchMapping(path = "/ubah-status/{id}")
     public boolean ubahStatus(@PathVariable Integer id, @RequestParam Boolean isNewHero) {
-        Optional<Hero> result = heroes.stream().filter(hero -> hero.getId() == id).findFirst();
-        if (result.isPresent()) {
-            result.get().setIsNewHero(isNewHero);
-            return true;
-        } else {
-            return false;
-        }
+
     }
 
     // delete
     @DeleteMapping(path = "/hapus/{id}")
     public Boolean hapusData(@PathVariable Integer id) {
-        Optional<Hero> result = heroes.stream().filter(hero -> hero.getId() == id).findFirst();
-        if (result.isPresent()) {
-            heroes.remove(result.get());
-            return true;
-        } else {
-            return false;
-        }
+
     }
 }
---
+++ src/main/java/com/belajar/belajarspring/service/HeroServiceImpl.java
@@ -0,0 +1,68 @@
+package com.belajar.belajarspring.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.belajar.belajarspring.model.Hero;
+
+// Service digunakan sebagai penanda bahwa ini adalah class service
+// Service ini di gunakan juga untuk depedency injection/DI
+@Service
+public class HeroServiceImpl implements HeroService {
+
+    public List<Hero> heroes = new ArrayList<>();
+    public int counter = 1;
+
+    // function return data dari controller di pindahkan
+    // ke class implements ini
+    @Override
+    public List<Hero> daftarHero() {
+        return heroes;
+    }
+
+    @Override
+    public Hero tambahDataHero(Hero request) {
+        request.setId(counter); // auto add id
+        heroes.add(request);
+        counter++; // auto add id
+        return request;
+    }
+
+    @Override
+    public boolean updateData(Hero request, Integer id) {
+        final Optional<Hero> result = heroes.stream().filter(hero -> hero.getId() == id).findFirst();
+        if (result.isPresent()) {
+            result.get().setName(request.getName());
+            result.get().setDamage(request.getDamage());
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean ubahStatus(Integer id, boolean isNewHero) {
+        Optional<Hero> result = heroes.stream().filter(hero -> hero.getId() == id).findFirst();
+        if (result.isPresent()) {
+            result.get().setIsNewHero(isNewHero);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean hapusData(Integer id) {
+        Optional<Hero> result = heroes.stream().filter(hero -> hero.getId() == id).findFirst();
+        if (result.isPresent()) {
+            heroes.remove(result.get());
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
